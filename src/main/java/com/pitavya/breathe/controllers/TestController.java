28f2529fe7558ec8f31aa3e23654d0021183d149
faiz-pitavya <faizrocks9211@gmail.com>
2020-08-09 10:23:06 +0530

    Added base Hit for empty reference

--- src/main/java/com/pitavya/breathe/controllers/TestController.java
+++ src/main/java/com/pitavya/breathe/controllers/TestController.java
@@ -1,14 +1,25 @@
 package com.pitavya.breathe.controllers;
 
+import org.jboss.logging.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+	private static Logger LOGGER = Logger.getLogger(TestController.class);
+
 	@RequestMapping(value = "/test")
 	public String testApi() {
+		LOGGER.info("testApi()");
 		return "Test SuccessFull for Pitavya-Breathe Api";
+	}
+
+	@RequestMapping(value = "/")
+	public String baseHit() {
+		LOGGER.info("baseHit()");
+
+		return "Up And Running Breathe Api";
 	}
 
 }
